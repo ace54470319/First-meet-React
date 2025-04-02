8278326baf3f9cd8c2b1b26f5b6412930847a973
m_SW319 <ace5447@naver.com>
2025-04-02 15:32:19 +0900

    mini-blog

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 
 import Clock from './Chapter_04/Clock';
 import CommentList from './Chapter_05/CommentList';
-import NotificationList from './Chapter_06/NotificationList'
+import NotificationList from './Chapter_06/NotificationList';
 import Accommodate from './Chapter_07/Accommodate';
 import ConfirmButton from './Chapter_08/ConfirmButton';
 import LandingPage from './Chapter_09/LandingPage';
 import AttendanceBook from './Chapter_10/AttendanceBook';
 import SignUp from './Chapter_11/SignUp';
+import Calculator from './Chapter_12/Calculator';
+import ProfileCard from './Chapter_13/ProfileCard';
+import Blocks from './Chapter_15/Blocks';
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      <Blocks />
     </div>
   );
 }
